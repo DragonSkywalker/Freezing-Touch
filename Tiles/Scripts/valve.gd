31eb9520532e23db89e303516a4b64306c9d90b5
Michael Wu <wurunzhi@hotmail.com>
2023-11-14 01:22:20 +0800

    Added balling
    and level 3 i guess (need bug fix)

--- Tiles/Scripts/valve.gd
+++ Tiles/Scripts/valve.gd
@@ -2,3 +2,9 @@ class_name Valve
 extends StaticBody2D
 
 var is_on = true
+
+func _process(delta):
+	var outlet = get_parent().get_node_or_null("Outlet")
+	if outlet:
+		outlet.is_on = is_on
+		print(outlet.is_on)
