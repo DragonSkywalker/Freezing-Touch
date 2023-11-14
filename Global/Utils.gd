241a6fe5cb0d144099db0cd8e2661c1a4495ce77
Michael Wu <wurunzhi@hotmail.com>
2023-11-14 18:40:48 +0800

    Made the game no longer relies on save.bin

--- Global/Utils.gd
+++ Global/Utils.gd
@@ -2,21 +2,22 @@ extends Node
 
 var level
 
-const SAVE_PATH = "res://Global/save.bin"
+#const SAVE_PATH = "res://Global/save.bin"
 
 func _ready():
-	loadGame()
+	level = 1
+#	loadGame()
 
 func saveGame(lvl: int):
 	level = lvl
-	var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
-	var jstr = JSON.stringify(lvl)
-	file.store_line(jstr)
+#	var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
+#	var jstr = JSON.stringify(lvl)
+#	file.store_line(jstr)
 
-func loadGame():
-	var file = FileAccess.open(SAVE_PATH, FileAccess.READ)	
-	if FileAccess.file_exists(SAVE_PATH):
-		if not file.eof_reached():
-			var current_line = JSON.parse_string(file.get_line())
-			if current_line:
-				level = int(current_line)
+#func loadGame():
+#	var file = FileAccess.open(SAVE_PATH, FileAccess.READ)	
+#	if FileAccess.file_exists(SAVE_PATH):
+#		if not file.eof_reached():
+#			var current_line = JSON.parse_string(file.get_line())
+#			if current_line:
+#				level = int(current_line)
